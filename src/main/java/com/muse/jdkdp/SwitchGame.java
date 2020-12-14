2e50c7f9f16a0662ae008d5ae507f4aa6697eb60
muse <i@leespy.com>
2020-12-14 08:56:39 +0800

    add

---
+++ src/main/java/com/muse/jdkdp/SwitchGame.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2020 Baidu, Inc. All Rights Reserved.
+ */
+package com.muse.jdkdp;
+
+/**
+ * Date 2020/10/24 10:46 上午
+ * Author lijinlong02@baidu.com
+ */
+public class SwitchGame implements Game {
+
+    @Override
+    public String play() {
+        return "play SwitchGame";
+    }
+}
